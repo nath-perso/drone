093028d22d3a1cc0157d5a5fc1187024b6f99c4a
Nathan Girard <nathan.girard@sfr.fr>
2023-01-19 10:24:50 +0100

    Make error codes inacccessible outside PS2.cpp

--- renegat_2/include/PS2_controller.h
+++ renegat_2/include/PS2_controller.h
@@ -11,28 +11,11 @@
  * ===                         DEFINE                           ===
  * ================================================================ */
 
-/* Configuration */
-#define PS2_PRESSURE  true
-#define PS2_RUMBLE    false
 
 /* ================================================================
  * ===                        VARIABLES                         ===
  * ================================================================ */
- /* Controller connection error codes */
-enum PS2_connection_error_code_list : byte {
-    no_error_code_k,
-    not_found_code_k,
-    no_command_code_k,
-    no_pressure_code_k
-};
 
-
-/* Controller type codes */
-enum PS2_controller_type_code_list : byte {
-    unknown_k,
-    dualshock_k,
-    guitar_hero_k
-};
 
 /* ================================================================
  * ===                        FUNCTIONS                         ===
